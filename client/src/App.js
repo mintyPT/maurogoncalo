a844fb69189b0278da78c3a78fae1c411db720de
Mauro  Santos <mauro.goncalo@gmail.com>
2016-12-30 15:23:43 +0100

    With express server

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import me from './me_tiny.jpeg';
 
-import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';  // eslint-disable-line no-unused-vars
 import './App.css';
-
 
 class App extends Component {
 
@@ -12,7 +11,6 @@ class App extends Component {
   //     <span className="btn btn-md btn-primary btn-outline">Get in touch</span>
   //   </div>
   // </div>
-
   // <br/>
   // <br/>
 
@@ -22,7 +20,7 @@ class App extends Component {
 
         <div className="row">
           <div className="col-xs-12 col-sm-6 col-sm-offset-3 ">
-            <svg className="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.428 6c6.018 0 8.414 10 13.143 10 2.115 0 3.48-1.578 3.48-4s-1.365-4-3.48-4c-1.664 0-2.86 1.07-4.016 2.395L13.31 8.833C14.81 7.3 16.36 6 18.57 6 21.768 6 24 8.455 24 12s-2.232 6-5.43 6C12.567 18 10.164 8 5.428 8c-2.113 0-3.48 1.578-3.48 4s1.366 4 3.48 4c1.664 0 2.86-1.068 4.015-2.392l1.244 1.56C9.187 16.7 7.637 18 5.427 18 2.232 18 0 15.545 0 12s2.23-6 5.428-6z"/></svg>
+            <img src={me} className="me" alt="Mauro Santos"/>
           </div>
         </div>
 
@@ -53,7 +51,7 @@ class App extends Component {
         <div className="row">
           <div className="col-xs-12 col-sm-6 col-sm-offset-3">
             <ul className="list-inline">
-              <li><a href="mailto:mauro.goncalo@gmail.com">mauro.goncalo@gmail.com</a></li>
+              <li><a href="mailto:mauro.goncalo@gmail.com">Say Hi</a></li>
               <li className="sep">▪︎</li>
               <li><a href="https://github.com/mintyPT">Github</a></li>
               <li className="sep">▪︎</li>
