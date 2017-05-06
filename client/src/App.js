42c82447aea3a2add7e657c66cbf9abe33e1c341
Unknown <mintypt@gmail.com>
2017-05-07 01:55:01 +0200

    v2

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,38 @@ import me from './me_tiny.jpeg';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';  // eslint-disable-line no-unused-vars
 import './App.css';
 
+
+const Footer = () => (
+  <footer>
+    <div className="row text-center">
+      <div className="col-xs-12 col-sm-6 col-sm-offset-3">
+        <ul className="list-inline">
+          <li><a href="mailto:mauro.goncalo@gmail.com">Say Hi</a></li>
+          <li className="sep">▪︎</li>
+          <li><a href="https://github.com/mintyPT">Github</a></li>
+          <li className="sep">▪︎</li>
+          <li><a href="https://twitter.com/mauro_goncalo">Twitter</a></li>
+          <li className="sep">▪︎</li>
+          <li><a href="https://medium.com/@mauro_goncalo">Medium</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+)
+
+
+
+
 class App extends Component {
 
-  // <div className="row">
-  //   <div className="col-xs-12 col-sm-6 col-sm-offset-3 ">
-  //     <span className="btn btn-md btn-primary btn-outline">Get in touch</span>
-  //   </div>
-  // </div>
-  // <br/>
-  // <br/>
-
   render() {
+
+    const SwissFlag = ({size}) => (<svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 32 32"><rect fill="#f00" height="32" width="32"/><rect fill="#fff" height="6" width="20" x="6" y="13"/><rect fill="#fff" height="20" width="6" x="13" y="6"/></svg>);
+
     return (
-      <div className="container home text-center">
+
+      <div>
+      <div className="container home text-center content">
 
         <div className="row">
           <div className="col-xs-12 col-sm-6 col-sm-offset-3 ">
@@ -39,9 +58,17 @@ class App extends Component {
         <div className="row">
           <div className="col-xs-12 col-sm-6 col-sm-offset-3  text">
             <p>
-              My name is <b>Mauro G. Santos</b>, Full-Stack PHP/Nodejs Developer. I like to build stuff on the side, which is why I'm currently working on <a href="http://blenditt.com">BlendItt</a> and <a href="https://psstcard.com">PsstCard</a>.
+              My name is <b>MG Santos</b>. I'm a Full-Stack Developer based in Switzerland <SwissFlag size={15}/>. I like to build stuff, take a look: <a href="http://blenditt.com">BlendItt</a>, <a href="https://psstcard.com">PsstCard</a> and  <a href="https://8config.com">8config</a>.
             </p>
-            <p><u>I'm currently looking for my next job.</u></p>
+          </div>
+        </div>
+
+
+        <br/>
+        <br/>
+        <div className="row">
+          <div className="col-xs-12 col-sm-6 col-sm-offset-3  text">
+            Looking for help, <a href="mailto:mauro.goncalo@gmail.com">how can I help you?</a>
           </div>
         </div>
 
@@ -51,17 +78,34 @@ class App extends Component {
         <div className="row">
           <div className="col-xs-12 col-sm-6 col-sm-offset-3">
             <ul className="list-inline">
-              <li><a href="mailto:mauro.goncalo@gmail.com">Say Hi</a></li>
-              <li className="sep">▪︎</li>
-              <li><a href="https://github.com/mintyPT">Github</a></li>
-              <li className="sep">▪︎</li>
-              <li><a href="https://twitter.com/mauro_goncalo">Twitter</a></li>
-              <li className="sep">▪︎</li>
-              <li><a href="https://medium.com/@mauro_goncalo">Medium</a></li>
+              <li>#nodejs</li>
+              <li>#javascript</li>
+              <li>#python</li>
+              <li>#php</li>
+              <li>#phalcon</li>
+              <li>#wordpress</li>
+              <li>#laravel</li>
+              <li>#react</li>
+              <li>#redux</li>
+              <li>#scrapping</li>
+              <li>#bots</li>
             </ul>
           </div>
         </div>
+
+        <br/>
+        <br/>
+
+
       </div>
+
+      <div className="container-fluid Footer">
+        <div className="container">
+          <Footer/>
+        </div>
+      </div>
+      </div>
+
     );
   }
 }
